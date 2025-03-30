7d1b90b8ffbf00b82920dd2a104480da58e2c714
Mike Rouse <mikerouse831@gmail.com>
2025-03-30 19:17:19 +0100

    file system restructure

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Political Canvasser</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <div id="game-container"></div>
+  
+  <!-- Load Phaser from CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/phaser@3/dist/phaser.js"></script>
+  <!-- Your game code -->
+  <script src="js/main.js"></script>
+</body>
+</html>
